f85f61175ea3e65b5d6f8aadeca2d3b4ec203cda
Arsany Wageeh <52182796+ArsanyWageehN@users.noreply.github.com>
2023-05-20 01:00:03 +0300

    Create BankingApplication.java

---
+++ BankingApplication.java
@@ -0,0 +1,21 @@
+
+package bankingapplication;
+
+public class BankingApplication extends Application {
+
+
+
+    private List<BankAccount> bankAccounts = new ArrayList<>();
+
+    private TextField accountNumberField, amountField, balanceField;
+
+    private Label statusLabel;
+
+     private  PasswordField passwordField;
+
+
+
+@Override
+
+public void start(Stage primaryStage) throws Exception {
+      }
